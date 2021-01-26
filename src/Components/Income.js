f6c12e018894c19e8c89f88ef582dc6f09746810
ligilv <ligilvjames1996@gmail.com>
2021-01-27 00:36:21 +0530

    input hides on clicking submit

--- src/Components/Income.js
+++ src/Components/Income.js
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 
 function Income() {
   const [income, setIncome] = useState();
+  const [hide, setHide] = useState(false);
   const [incometrack, setIncomeTrack] = useState();
   const trackinput = (e) => {
     setIncomeTrack(e.target.value);
-    console.log(incometrack);
   };
   const setit = (e) => {
     e.preventDefault();
     setIncome(incometrack);
+    setHide(!hide);
   };
   return (
     <div>
       <form>
         <input
+          //conditiional rendering in style input when income is added it get hidden
+          style={{ display: hide ? "none" : "" }}
           type="number"
           placeholder="Please enter your Income"
           onChange={trackinput}
@@ -23,7 +26,11 @@ function Income() {
           add
         </button>
       </form>
-      {incometrack >= 0 ? <h1>{income}</h1> : <h1>Please add valid Income</h1>}
+      {incometrack >= 0 || "" ? (
+        <h1>Income is {incometrack} USD</h1>
+      ) : (
+        <h1>Please add valid Income</h1>
+      )}
       {/* Income is {income} USD */}
     </div>
   );
